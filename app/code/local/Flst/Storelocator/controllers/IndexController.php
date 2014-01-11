a557cf35c5e03b3606af8ecbc2cc0828e939f869
Valent1Polo <me@valentinpolo.fr>
2014-01-11 10:55:32 +0100

    Bases du module.

---
+++ app/code/local/Flst/Storelocator/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+class Flst_Storelocator_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'Méthode par défaut';
+    }
+    
+    public function helloAction()
+    {
+        echo 'Méthode Helo!';
+    }
+}
